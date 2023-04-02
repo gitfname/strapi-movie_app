52cfddb324e54849a8b328b1f7acd82f66fc3da3
gitfname <newamailnam@gmail.com>
2023-04-02 11:54:28 +0430

    commit 2

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,20 @@
 module.exports = {
     "generate-data": {
         enabled: true,
+    },
+    
+    "upload": {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET,
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
     }
 }
