7d03d205de0a30528753a239746cb36f98077c0b
gitfname <newamailnam@gmail.com>
2023-04-23 17:46:20 +0430

    'isUserExistsInTvSeriesCommentReplyLike' policy is added

--- src/api/tv-show-comment-reply-like/routes/tv-show-comment-reply-like.js
+++ src/api/tv-show-comment-reply-like/routes/tv-show-comment-reply-like.js
@@ -9,7 +9,7 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 module.exports = createCoreRouter('api::tv-show-comment-reply-like.tv-show-comment-reply-like', {
     config: {
         create: {
-            policies: ["global::isUserExists"],
+            policies: ["global::isUserExistsInTvSeriesCommentReplyLike"],
             middlewares: ["global::injectOwnerId"]
         },
         update: {
